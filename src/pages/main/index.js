8777f1e711a020eba54f3ee39e9592169aafd833
Victor Hermes <me.victorhermes@gmail.com>
2018-12-10 19:54:47 -0200

    Configuração do Redux

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,30 @@
 import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as FavoriteActions from "../../store/actions/favorites";
 
-export default class Main extends Component {
+import PropTypes from "prop-types";
+
+class Main extends Component {
+    static propTypes = {
+        addFavorite: PropTypes.func.isRequired,
+        favorites: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number,
+                name: PropTypes.string,
+                description: PropTypes.string,
+                url: PropTypes.string
+            })
+        ).isRequired
+    };
+
     state = {
         repositoryInput: ""
+    };
+
+    handleAddRepository = event => {
+        event.preventDefault();
+        this.props.addFavorite();
     };
 
     render() {
@@ -21,16 +43,29 @@ export default class Main extends Component {
                 </form>
 
                 <ul>
-                    <li>
-                        <p>
-                            <strong>facebook/react</strong> (A declarative,
-                            efficient, and flexible JavaScript library for
-                            building user interfaces.)
-                        </p>
-                        <a href="https://github.com/facebook/react">Github</a>
-                    </li>
+                    {this.props.favorites.map(favorite => (
+                        <li key={favorite.id}>
+                            <p>
+                                <strong>{favorite.name}</strong> (
+                                {favorite.description})
+                            </p>
+                            <a href={favorite.url}>Github</a>
+                        </li>
+                    ))}
                 </ul>
             </Fragment>
         );
     }
 }
+
+const mapStateToProps = state => ({
+    favorites: state.favorites
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(FavoriteActions, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Main);
