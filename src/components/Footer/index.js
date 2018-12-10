22ba4dfd46543f8832b48871485d2399691e5fed
Victor Hermes <me.victorhermes@gmail.com>
2018-12-10 20:10:35 -0200

    Criando footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import propTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Footer = ({ count }) => <p>Você tem {count} favoritos.</p>;
+
+Footer.prototype = {
+    count: propTypes.number.isRequired
+};
+
+const mapStateToProps = state => ({
+    count: state.favorites.length
+});
+
+export default connect(mapStateToProps)(Footer);
