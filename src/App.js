ce86d9f6866eb7cfcb65a8810d015800a39f815c
Victor Hermes <me.victorhermes@gmail.com>
2019-05-08 17:59:20 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
-import store from "./store";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-    <Provider store={store}>
-        <h1>Hello fucking world!</h1>
-    </Provider>
+  <Provider store={store}>
+    <h1>Hello fucking world!</h1>
+  </Provider>
 );
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { CreateStore, createStore } from "redux";
+import { CreateStore, createStore } from 'redux';
+
 const store = createStore(() => {});
 
 export default store;
