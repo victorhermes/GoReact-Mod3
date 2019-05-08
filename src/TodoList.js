c4cc4c4a0a0f74f5e12afd361bd453759aa6ac11
Victor Hermes <me.victorhermes@gmail.com>
2019-05-08 17:58:29 -0300

    att

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,42 +1,39 @@
-import React, { Fragment } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
-import * as TodoActions from "./store/actions/todos";
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
 
 const TodoList = ({ todos, addTodo, removeTodo }) => (
-    <Fragment>
-        <ul>
-            {todos.map(todo => (
-                <li key={todo.id}>
-                    <button onClick={() => removeTodo(todo.id)}>X</button>
-                    {todo.text}
-                </li>
-            ))}
-        </ul>
-        <button onClick={() => addTodo("Fazer caf")}> Enviar</button>
-    </Fragment>
+  <Fragment>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <button onClick={() => removeTodo(todo.id)}>X</button>
+          {todo.text}
+        </li>
+      ))}
+    </ul>
+    <button onClick={() => addTodo('Fazer caf')}> Enviar</button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
-    addTodo: PropTypes.func.isRequired,
-    removeTodo: PropTypes.func.isRequired,
-    todos: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number,
-            text: PropTypes.string
-        })
-    ).isRequired
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+  })).isRequired,
 };
 
 const mapStateToProps = state => ({
-    todos: state.todos
+  todos: state.todos,
 });
 
-const mapDispatchToProps = dispatch =>
-    bindActionCreators(TodoActions, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps,
 )(TodoList);
